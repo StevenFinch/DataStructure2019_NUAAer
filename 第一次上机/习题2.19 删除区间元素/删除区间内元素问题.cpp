7ce56219c978f11434e4f9fd547fcf9cc5957678
Steven Song <geekddx@gmail.com>
2019-10-06 15:03:47 +0800

    更新顺序

---
+++ 第一次上机/习题2.19 删除区间元素/删除区间内元素问题.cpp
@@ -0,0 +1,99 @@
+/***************************************
+ **   Filename£º É¾³ýÇø¼äÔªËØ.cpp
+ **
+ **   Compiler:  Visual Studio 2019
+ **
+ **   StudentID: 1618*****
+ **
+ **   Name:      Steven
+ **
+ ***************************************/
+
+#include <iostream>
+
+using namespace std;
+typedef int ElemType;
+typedef struct LNode
+{
+	ElemType data;
+	LNode* next;
+}LNode, * LinkList;
+typedef struct
+{
+	LinkList head;
+	LinkList tail;
+	LNode* pCurNode;
+	int length;
+}ListInfo;
+bool InitList(ListInfo* List)
+{
+	List->head = NULL;
+	List->head = (LNode*)malloc(sizeof(ListInfo));
+	if (NULL == List->head)
+		exit(OVERFLOW);
+	List->head->next = NULL;
+	List->tail = List->head;
+	List->pCurNode = List->head;
+	List->length = 0;
+	while (List->length < 10)
+	{
+		int data;
+		cin >> data;
+		LNode* newNode = (LNode*)malloc(sizeof(LNode));
+		if (NULL == newNode)
+			exit(OVERFLOW);
+		newNode->data = data;
+		newNode->next = NULL;
+
+		List->tail->next = newNode;
+		List->tail = newNode;
+		List->length++;
+	}
+	return true;
+}
+bool TraverseList(ListInfo* List)
+{
+	LNode* p = List->head->next;
+	while (p != NULL)
+	{
+		cout << p->data << ' ';
+		p = p->next;
+	}
+	return true;
+}
+bool DeleteElem(ListInfo* List, int mink, int maxk)
+{
+	if (List->head == NULL || List->head->next == NULL)
+	{
+		return false;
+	}
+	LNode* p = List->head;
+	LNode* q = List->head->next;
+	LNode* r = NULL;
+	while (q && q->data <= mink)
+	{
+		p = q;
+		q = q->next;
+	}
+	if (q)
+	{
+		while (q->data < maxk)
+		{
+			r = q;
+			p->next = q->next;
+			q = q->next;
+			free(r);
+		}
+		return true;
+	}
+	return false;
+}
+int main()
+{
+	ListInfo List;
+	InitList(&List);
+	DeleteElem(&List, 3, 8);
+	TraverseList(&List);
+	system("pause");
+	return 0;
+}
