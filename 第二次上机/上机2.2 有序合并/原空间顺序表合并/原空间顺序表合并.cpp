72c987c60b2493c676fca672d4e5a27d5f1307cc
Steven Song <geekddx@gmail.com>
2019-10-20 15:10:43 +0800

    1

--- 第二次上机/上机2.2 有序合并/原空间顺序表合并/原空间顺序表合并.cpp
+++ 第二次上机/上机2.2 有序合并/原空间顺序表合并/原空间顺序表合并.cpp
@@ -1,5 +1,5 @@
 /***************************************
- **   Filename： SequenceList.cpp
+ **   Filename： 原空间顺序表合并.cpp
  **
  **   Compiler:  Visual Studio 2019
  **
@@ -15,8 +15,8 @@
 
 using namespace std;
 
-const int LIST_INIT_SIZE = 256;	//顺序表初始容量
-const int LIST_INCREMENT = 256;	//顺序表增加容量
+const int LIST_INIT_SIZE = 256;
+const int LIST_INCREMENT = 256;
 const int OK = 1;
 const int ERROR = 0;
 const int YES = 1;
@@ -33,19 +33,17 @@ typedef struct
 
 Status InitList(SeqList* List, string filename)
 {
-	List->pElem = NULL;	//分配内存之前必须将指针设置为NULL，否则为野指针，后续无法判断内存是否分配成功。
+	List->pElem = NULL;	
 	List->pElem = (ElemType*)malloc(LIST_INIT_SIZE * sizeof(ElemType));
-	//检验分配内存是否成功
 	if (NULL == List->pElem)
 		exit(OVERFLOW);
-	for (int i = 0; i < LIST_INIT_SIZE; i++)	//分配内存成功，顺序表内容初始化
+	for (int i = 0; i < LIST_INIT_SIZE; i++)	
 	{
 		List->pElem[i] = -1;
 	}
 	List->length = 0;
 	List->size = LIST_INIT_SIZE;
 
-	//读取数据
 	ifstream inFile(filename);
 	if (!inFile)
 	{
@@ -65,7 +63,7 @@ Status DestoryList(SeqList* List)
 	if (NULL == List->pElem)
 	{
 		free(List->pElem);
-		List->pElem = NULL;	//释放内存之后必须将指针设置为NULL，否则成为野指针。
+		List->pElem = NULL;	
 	}
 	List->size = 0;
 	List->length = 0;
@@ -86,22 +84,22 @@ Status ListTraverse(SeqList* List)
 }
 Status InsertElem(SeqList* List, int logicalPos, ElemType elem)
 {
-	//合法性检验
+
 	if (logicalPos > 0 && logicalPos < List->length + 1)
 	{
-		//存储空间大小检验
+
 		if (List->length >= List->size)
 		{
 			List->pElem = (ElemType*)realloc(List->pElem, (List->size + LIST_INCREMENT) * sizeof(ElemType));
-			if (NULL == List->pElem)	//检查内存分配是否成功
+			if (NULL == List->pElem)	
 			{
 				cout << "Reallocating failed";
 				exit(OVERFLOW);
 			}
 			List->size = List->size + LIST_INCREMENT;
 		}
-		ElemType* q = &List->pElem[logicalPos - 1];	//q为插入位置
-		for (ElemType* p = &List->pElem[List->length - 1]; p >= q; p--)	//p为移动指针
+		ElemType* q = &List->pElem[logicalPos - 1];	
+		for (ElemType* p = &List->pElem[List->length - 1]; p >= q; p--)	
 		{
 			*(p + 1) = *p;
 		}
@@ -140,7 +138,7 @@ void GetUnionOfTwoLists(SeqList* l1, SeqList* l2)
 	}
 	return;
 }
-//主函数（测试函数）
+
 int main()
 {
 	SeqList l1;
